90694b8b146f9a6158a6447815c6ea3d32f8bc72
Noah Berthusen <nfberthusen@gmail.com>
2019-09-15 23:50:16 -0500

    adding a lot of authentication infrastructure in the backend

--- Backend/MyVirtualCloset/MyVirtualCloset/Controllers/ValuesController.cs
+++ Backend/MyVirtualCloset/MyVirtualCloset/Controllers/ValuesController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyVirtualCloset.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
---
+++ Backend/MyVirtualCloset/MyVirtualCloset/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVirtualCloset.Api
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddTransient<Core.Auth.IAuthService, Infrastructure.Auth.AuthService>();
+
+            return services;
+        }
+    }
+}
