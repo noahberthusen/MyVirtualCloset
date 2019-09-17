179c37b243f81d998e71759ea52f270c997f377b
mambourn <mambourn@iastate.edu>
2019-09-17 01:00:46 -0500

    frontend auth setup

--- Frontend/virtualCloset/src/app/app.routes.ts
+++ Frontend/virtualCloset/src/app/app.routes.ts
@@ -1,15 +1,34 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from '@src/app/home/home.component';
+import { LoginComponent } from './login';
+import { AuthGuard } from './_guards';
 
-export const routes: Routes = [
+// export const routes: Routes = [
+//   {
+//       path: '',
+//       redirectTo: '/home',
+//       pathMatch: 'full',
+//   },
+//   {
+//       path: 'home',
+//       component: HomeComponent,
+//   },
+// ];
+
+const appRoutes: Routes = [
   {
       path: '',
-      redirectTo: '/home',
-      pathMatch: 'full',
+      component: HomeComponent,
+      canActivate: [AuthGuard]
   },
   {
-      path: 'home',
-      component: HomeComponent,
+      path: 'login',
+      component: LoginComponent
   },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
 ];
+
+export const routing = RouterModule.forRoot(appRoutes);
