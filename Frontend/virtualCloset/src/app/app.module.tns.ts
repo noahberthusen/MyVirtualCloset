0fc93cec3f39338e16d19c87cd4b55e836730d4d
mambourn <mambourn@iastate.edu>
2019-09-21 18:23:15 +0000

    how to use db software

--- Frontend/virtualCloset/src/app/app.module.tns.ts
+++ Frontend/virtualCloset/src/app/app.module.tns.ts
@@ -4,6 +4,7 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { LoginComponent } from '@src/app/login/login.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -16,6 +17,7 @@ import { HomeComponent } from '@src/app/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     NativeScriptModule,
