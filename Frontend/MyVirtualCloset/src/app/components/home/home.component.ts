4dff4be6a519b4ff839c8e326e20245459f9f373
mambourn <mambourn@iastate.edu>
2019-09-25 16:51:27 -0500

    initial button for picture

--- Frontend/MyVirtualCloset/src/app/components/home/home.component.ts
+++ Frontend/MyVirtualCloset/src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//angular material requires installation first: ng add @angular/material
+// import {MatButtonModule} from '@angular/material/button'; //angular material feature for using buttons
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
