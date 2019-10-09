7ee8d35c91405d9923d857cfa32c8ae5beef27cc
Noah Berthusen <nfberthusen@gmail.com>
2019-10-08 22:56:47 -0500

    it didn't keep my changes

--- Frontend/MyVirtualCloset/src/app/services/images.service.spec.ts
+++ Frontend/MyVirtualCloset/src/app/services/images.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ImagesService } from './images.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ImagesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: ImagesService = TestBed.get(ImagesService);
