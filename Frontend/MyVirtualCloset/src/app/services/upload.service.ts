a4956f7254655c25181818ca5576cd768cf4db85
mambourn <mambourn@iastate.edu>
2019-10-16 21:13:34 -0500

    work in progress

--- Frontend/MyVirtualCloset/src/app/services/upload.service.ts
+++ Frontend/MyVirtualCloset/src/app/services/upload.service.ts
@@ -14,8 +14,9 @@ export class UploadService {
     const formData = new FormData();
     console.log(image);
     formData.append('image', image);
-    // formData.append('tags',"blue");
-    // formData.append('name',"shirt");
+    formData.append('tags',"blue");
+    formData.append('name',"shirt");
+    // console.log("gets here"+formData.get('image'));
     return this.http.post<Image>('https://localhost:44383/api/ClothingItem/add', formData);
   }
 }
