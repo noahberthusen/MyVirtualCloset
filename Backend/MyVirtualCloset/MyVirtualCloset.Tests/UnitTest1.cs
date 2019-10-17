39cdbb5a9e719bccbafdfb42087040911fc1baae
boyle121 <boyle121@iastate.edu>
2019-10-17 11:11:43 -0500

    test

---
+++ Backend/MyVirtualCloset/MyVirtualCloset.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyVirtualCloset.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
