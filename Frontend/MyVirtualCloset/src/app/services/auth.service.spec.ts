46e8ccbecb640becf405b53c811041a5baf786ed
mambourn <mambourn@iastate.edu>
2019-10-21 18:16:07 -0500

    mock tests

--- Frontend/MyVirtualCloset/src/app/services/auth.service.spec.ts
+++ Frontend/MyVirtualCloset/src/app/services/auth.service.spec.ts
@@ -1,15 +1,102 @@
 import { TestBed } from '@angular/core/testing';
-
+import { User } from '../models/User';
 import { AuthService } from './auth.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ]
-  }));
+  let service: AuthService;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [ 
+        HttpClientTestingModule      
+      ]
+    })
+  
+    service = TestBed.get(AuthService);
+  });
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+  
+  it('should expect a user', () => {
+    const spy = spyOn(service, 'login').and.returnValue(
+      of({
+        id: '',
+        firstName: 'S',
+        lastName: 'Mitra',
+        username: 'smitra',
+        email: 'smitra@iastate.edu',
+        password: '',
+        role: 'User',
+        token: '',
+        salt: '',
+        hash: ''
+      })
+    )
+    service.login(new User()).subscribe(data => {
+      expect(data.username).toBe('smitra');
+      expect(data.lastName).toBe('Mitra');
+      expect(data.role).toBe('User');
+    });
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  
+  it('tests forgotPassword', () => {
+    const spy = spyOn(service, 'forgotPassword').and.returnValue(
+      of({
+        id: '',
+        firstName: 'S',
+        lastName: 'Mitra',
+        username: 'smitra',
+        email: 'smitra@iastate.edu',
+        password: '',
+        role: 'User',
+        token: '',
+        salt: '',
+        hash: ''
+      })
+    )
+    service.forgotPassword(new User()).subscribe(data => {
+      expect(data.username).toBe('smitra');
+      expect(data.lastName).toBe('Mitra');
+      expect(data.role).toBe('User');
+    });
+    expect(spy).toHaveBeenCalled();
+  });
+
+    it('tests changePassword', () => {
+      const spy = spyOn(service, 'changePassword').and.returnValue(
+        of({
+          id: '',
+          firstName: 'S',
+          lastName: 'Mitra',
+          username: 'smitra',
+          email: 'smitra@iastate.edu',
+          password: '',
+          role: 'User',
+          token: '',
+          salt: '',
+          hash: ''
+        })
+      )
+      service.changePassword(new User()).subscribe(data => {
+        expect(data.username).toBe('smitra');
+        expect(data.lastName).toBe('Mitra');
+        expect(data.role).toBe('User');
+      });
+      expect(spy).toHaveBeenCalled();
+    });
+
+
+  
+
 });
