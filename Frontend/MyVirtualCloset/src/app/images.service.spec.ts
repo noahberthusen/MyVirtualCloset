5b7609249fd4ca61a17b77c15f82e907c9c14d9a
KayAnne Bryant <kebryant@iastate.edu>
2019-10-22 01:50:05 -0500

    didnt merge everything

---
+++ Frontend/MyVirtualCloset/src/app/images.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ImagesService } from './images.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('ImagesService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
+
+  it('should be created', () => {
+    const service: ImagesService = TestBed.get(ImagesService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ Frontend/MyVirtualCloset/src/app/images.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Image } from '../models/Image';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesService {
+
+  private currentImageSubject: BehaviorSubject<Image>;
+  public currentImage: Observable<Image>;
+
+  constructor(private http: HttpClient) {
+    this.currentImageSubject = new BehaviorSubject<Image>(JSON.parse(localStorage.getItem('currentImage')));
+    this.currentImage = this.currentImageSubject.asObservable();
+  }
+
+  public get currentImageValue(): Image {
+    return this.currentImageSubject.value;
+  }
+
+  displayImages() {
+    console.log("inside clothing service");
+    return this.http.get<Image>('http://coms-309-ks-7.misc.iastate.edu:8080/api/ClothingItem/viewAllUserClothes')
+      .pipe(map(images => {
+        console.log("inside http get request");
+        console.log(images);  
+        return images;
+      }));
+  }
+}
