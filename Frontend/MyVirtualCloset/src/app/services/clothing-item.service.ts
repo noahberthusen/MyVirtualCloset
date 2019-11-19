1eb226d9467c55692f5e3391cfc8a0fffd379624
Noah Berthusen <nfberthusen@gmail.com>
2019-11-19 00:25:38 -0600

    i think it works now

--- Frontend/MyVirtualCloset/src/app/services/clothing-item.service.ts
+++ Frontend/MyVirtualCloset/src/app/services/clothing-item.service.ts
@@ -14,7 +14,7 @@ export class ClothingItemService {
   viewAllUsersClothes() {
     console.log("inside view all users clothes function");
     let clothing: ClothingItem[] = [];
-    return this.http.get<ClothingItem[]>('https://localhost:44383/api/ClothingItem/viewAllUserClothes')
+    return this.http.get<ClothingItem[]>('http://coms-309-ks-7.misc.iastate.edu:8080/api/ClothingItem/viewAllUserClothes')
     .pipe(map(res => {
       res.forEach(obj => {
         let image = new ClothingItem();
@@ -39,7 +39,7 @@ export class ClothingItemService {
 
     formData.append('tags', this.tags);
 
-    return this.http.post<ClothingItem[]>('https://localhost:44383/api/ClothingItem/search', formData)
+    return this.http.post<ClothingItem[]>('http://coms-309-ks-7.misc.iastate.edu:8080/api/ClothingItem/search', formData)
     .pipe(map(res => {
       let clothing: ClothingItem[] = [];
       res.forEach(obj => {
@@ -60,7 +60,7 @@ export class ClothingItemService {
     const formData = new FormData();
     formData.append('id', myId);
 
-    return this.http.post<ClothingItem>('https://localhost:44383/api/ClothingItem/getById', formData)
+    return this.http.post<ClothingItem>('http://coms-309-ks-7.misc.iastate.edu:8080/api/ClothingItem/getById', formData)
     .pipe(map(res => {
       let cloth = new ClothingItem();
       cloth.name = res.name;
