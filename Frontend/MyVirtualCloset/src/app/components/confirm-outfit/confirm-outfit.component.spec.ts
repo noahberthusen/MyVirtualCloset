1484a53374259a5f041ae0f050728f279b4bca20
mambourn <mambourn@iastate.edu>
2019-11-18 22:31:31 -0600

    passes tests and builds

--- Frontend/MyVirtualCloset/src/app/components/confirm-outfit/confirm-outfit.component.spec.ts
+++ Frontend/MyVirtualCloset/src/app/components/confirm-outfit/confirm-outfit.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfirmOutfitComponent } from './confirm-outfit.component';
 
-// describe('ConfirmOutfitComponent', () => {
-//   let component: ConfirmOutfitComponent;
-//   let fixture: ComponentFixture<ConfirmOutfitComponent>;
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from 'src/app/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ConfirmOutfitComponent ]
-//     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ConfirmOutfitComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('ConfirmOutfitComponent', () => {
+  let component: ConfirmOutfitComponent;
+  let fixture: ComponentFixture<ConfirmOutfitComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [
+            HttpClientTestingModule,
+            ReactiveFormsModule,
+            RouterTestingModule,
+            MaterialModule,
+            BrowserAnimationsModule
+        ],
+      declarations: [ ConfirmOutfitComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmOutfitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('form should be invalid', async(() => {
+    component.userInput.controls['outfitName'].setValue('');
+    component.userInput.controls['description'].setValue('');
+    expect(component.userInput.valid).toBeFalsy();
+  }));
+
+  it('form should be valid', async(() => {
+    component.userInput.controls['outfitName'].setValue('myOutfit');
+    component.userInput.controls['description'].setValue('Some description');
+    expect(component.userInput.valid).toBeTruthy();
+  }));
+
+
+
+
+});
--- Frontend/MyVirtualCloset/src/app/services/upload-outfit.service.spec.ts
+++ Frontend/MyVirtualCloset/src/app/services/upload-outfit.service.spec.ts
@@ -1,18 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UploadOutfitService } from './upload-outfit.service';
+import { Outfit } from 'src/app/models/Outfit';
+import { of } from 'rxjs';
 
-// describe('UploadOutfitService', () => {
-//   let component: UploadOutfitService;
-//   let fixture: ComponentFixture<UploadOutfitService>;
 
-//   beforeEach(() => 
-//     TestBed.configureTestingModule({
-//       declarations: [ UploadOutfitService ]
-//     }));
+describe('UploadOutfitService', () => {
+  let service: UploadOutfitService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UploadOutfitService],
+      imports: [ 
+        HttpClientTestingModule      
+      ]
+    })
+    service = TestBed.get(UploadOutfitService);
+  });
 
-//   it('should be created', () => {
-//     const service: UploadOutfitService = TestBed.get(UploadOutfitService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    const service: UploadOutfitService = TestBed.get(UploadOutfitService);
+    expect(service).toBeTruthy();
+  });
+
+});
